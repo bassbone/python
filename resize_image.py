f27bf9d277246a2f5889b55898e452a05946ea6b
bassbone <bassbone0o_o0@yahoo.co.jp>
2016-07-27 21:10:39 +0900

    resize_image.py

---
+++ resize_image.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import os, glob, Image
+
+from_dir = "/tmp/download/"
+to_dir = "/tmp/resize/"
+
+def getdirs(path):
+    dirs = []
+    for item in os.listdir(path):
+        if os.path.isdir(os.path.join(path, item)):
+            dirs.append(item)
+    return dirs
+
+
+for dir in getdirs(from_dir):
+    if not os.path.exists(to_dir + dir):
+        os.mkdir(to_dir + dir)
+    for file in glob.glob(from_dir + dir + "/*.jpg"):
+        try:
+            print file.rsplit("/")[4]
+            Image.open(file).resize((96,96)).save(to_dir + dir + "/" + file.rsplit('/')[4])
+        except:
+            continue
